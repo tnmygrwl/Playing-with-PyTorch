9ace9c93823010a91cdf9206c2d57d3dc10d821f
Tanmay Agrawal <tanmay7270@gmail.com>
2019-01-09 14:56:46 -0800

    improve inference fashion_MNIST.py

--- fashion_MNIST.py
+++ fashion_MNIST.py
@@ -38,23 +38,61 @@ class Classifier(nn.Module):
 
 model = Classifier()
 criterion = nn.NLLLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.0026)
+optimizer = optim.Adam(model.parameters(), lr=0.003)
 
-epochs = 10
+epochs = 30
+steps = 0
 
+train_losses, test_losses = [], []
 for e in range(epochs):
     running_loss = 0
     for images, labels in trainloader:
-        output = model(images)
-        loss = criterion(output, labels)
 
         optimizer.zero_grad()
+
+        log_ps = model(images)
+        loss = criterion(log_ps, labels)
         loss.backward()
         optimizer.step()
-        running_loss += loss.item()
-    else:
-        print(f"Training loss: {running_loss}")
 
-# find probabilities
+        running_loss += loss.item()
+
+    else:
+        test_loss = 0
+        accuracy = 0
+
+        with torch.no_grad():
+            for images, labels in testloader:
+                log_ps = model(images)
+                test_loss += criterion(log_ps, labels)
+
+                ps = torch.exp(log_ps)
+                top_p, top_class = ps.topk(1, dim=1)
+                equals = top_class == labels.view(*top_class.shape)
+                accuracy += torch.mean(equals.type(torch.FloatTensor))
+
+        train_losses.append(running_loss / len(trainloader))
+        test_losses.append(test_loss / len(testloader))
+
+        print("Epoch: {}/{}.. ".format(e + 1, epochs),
+              "Training Loss: {:.3f}.. ".format(running_loss / len(trainloader)),
+              "Test Loss: {:.3f}.. ".format(test_loss / len(testloader)),
+              "Test Accuracy: {:.3f}".format(accuracy / len(testloader)))
+
+# Inference
+
+model.eval()
+
+dataiter = iter(testloader)
+images, labels = dataiter.next()
+img = images[0]
+# Convert 2D image to 1D vector
+img = img.view(1, 784)
+
+# Calculate the class probabilities (softmax) for img
+with torch.no_grad():
+    output = model.forward(img)
+
+ps = torch.exp(output)  # for images[0]
 
 torch.exp(model(images[2].resize_(1, 784)))  # for images[2]
